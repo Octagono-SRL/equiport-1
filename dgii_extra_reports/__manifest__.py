a250e54c2eb6b91e514fed02dc8e7dca6d2fcf06
Wander Feliz <55289565+WanderFeliz-Octagono@users.noreply.github.com>
2022-02-10 14:47:25 -0400

    Update __manifest__.py

--- dgii_extra_reports/__manifest__.py
+++ dgii_extra_reports/__manifest__.py
@@ -13,7 +13,7 @@
     ],
     'data': [
         'views/dgii_report_views.xml',
-        'views/account_account_views.xml'
+        # 'views/account_account_views.xml'
     ],
     'installable': True,
     'auto_install': False
