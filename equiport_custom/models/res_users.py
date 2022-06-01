c2ecbdd19998c11fe7e673c67068b94d39300187
Wander Xavier Paniagua Candelario <55289565+WanderFeliz-Octagono@users.noreply.github.com>
2022-06-01 16:37:18 -0400

    added button clear access group

--- equiport_custom/models/res_users.py
+++ equiport_custom/models/res_users.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api
+from odoo.exceptions import ValidationError
 
 
 class ResUsers(models.Model):
@@ -6,3 +7,14 @@ class ResUsers(models.Model):
 
     allowed_stock_location_ids = fields.Many2many(string="Ubicaciones permitidas", comodel_name='stock.location', relation='user_allowed_stock_location_rel')
     allowed_stock_warehouse_ids = fields.Many2many(string="Almacenes permitidos", comodel_name='stock.warehouse', relation='user_allowed_stock_warehouse_rel')
+
+    def clear_user_groups(self):
+        if self == self.env.ref('base.user_admin') or self == self.env.user:
+            raise ValidationError("No puede realizar esta accion en el usuario logueado")
+        elif self.has_group('base.group_system') or self.has_group('base.group_erp_manager'):
+            raise ValidationError("No puede realizar esta accion en usuarios admin")
+        else:
+            self.update({
+                'groups_id': False
+            })
+
